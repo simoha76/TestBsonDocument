06c5b0c1ca83e875d73c77b91dca239d379dfd77
mohammed <aminchar@gmail.com>
2023-06-02 15:23:45 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using TestBsonDocument.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestBsonDocument.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public Product Get()
+        {
+            Product prod = new Product();
+            prod.ProductName = "Test";
+            prod.ProductCount = 20;
+            var doc = new BsonDocument
+            {
+                { "metadata1", "val1" }
+            };
+            prod.ProductMetadata = doc;
+            return prod;
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using System.Text.Json;
+
+namespace TestBsonDocument.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+        public BsonDocument ProductMetadata { get; set; }
+
+    }
+}
